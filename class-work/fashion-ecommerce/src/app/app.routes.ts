6e0b533e3fd0e55d421768133112bd4be7398142
Lyan <lyannahas@gmail.com>
2026-07-26 02:23:41 +0300

    feat: add unauthorized error page

--- class-work/fashion-ecommerce/src/app/app.routes.ts
+++ class-work/fashion-ecommerce/src/app/app.routes.ts
@@ -2,10 +2,11 @@ import { Routes } from '@angular/router';
 
 import { Login } from './features/auth/login/login';
 import { Signup } from './features/auth/signup/signup';
+import { Unauthorized } from './features/errors/unauthorized/unauthorized';
 import { Home } from './features/home/home';
+import { Men } from './features/shop/men/men';
 import { Shop } from './features/shop/shop';
 import { Women } from './features/shop/women/women';
-import { Men } from './features/shop/men/men';
 
 export const routes: Routes = [
   {
@@ -37,6 +38,12 @@ export const routes: Routes = [
     path: 'shop/men',
     component: Men
   },
+  {
+    path: 'unauthorized',
+    component: Unauthorized
+  },
+
+  // This must always remain last.
   {
     path: '**',
     redirectTo: 'login'
